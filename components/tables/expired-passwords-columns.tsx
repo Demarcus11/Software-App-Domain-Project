e9a9b1c706bf6d6a9166d33cc47cab02c836944b
Demarcus Hollman <Demarcushollman1@gmail.com>
2025-02-21 15:25:32 -0500

    UPDATE: files

--- components/tables/expired-passwords-columns.tsx
+++ components/tables/expired-passwords-columns.tsx
@@ -56,32 +56,6 @@ export const ExpiredPasswordsColumns = (windowSize: number) => {
         return <span>{date.toLocaleDateString()}</span>;
       },
     },
-    {
-      id: "actions",
-      header: "Actions",
-      cell: ({ row }) => {
-        const expiredPassword = row.original;
-
-        return (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="h-8 w-8 p-0">
-                <span className="sr-only">Open menu</span>
-                <MoreHorizontal className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuItem>
-                <Link href={`/expired-passwords/${expiredPassword.id}`}>
-                  Email
-                </Link>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        );
-      },
-    },
   ];
 
   const desktopColumns: ColumnDef<ExpiredPasswords>[] = [
@@ -152,32 +126,6 @@ export const ExpiredPasswordsColumns = (windowSize: number) => {
         const expiredPassword = row.original;
         const date = new Date(expiredPassword.passwordExpiresAt);
         return <span>{date.toLocaleDateString()}</span>;
-      },
-    },
-    {
-      id: "actions",
-      header: "Actions",
-      cell: ({ row }) => {
-        const expiredPassword = row.original;
-
-        return (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="h-8 w-8 p-0">
-                <span className="sr-only">Open menu</span>
-                <MoreHorizontal className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuItem>
-                <Link href={`/expired-passwords/${expiredPassword.id}`}>
-                  Email
-                </Link>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        );
       },
     },
   ];
