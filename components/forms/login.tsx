b19dead885b5d0aa7515a60d781075d7f224af54
Demarcus Hollman <Demarcushollman1@gmail.com>
2025-02-21 15:18:34 -0500

    UPDATE: files

--- components/forms/login.tsx
+++ components/forms/login.tsx
@@ -185,19 +185,19 @@ const LoginForm = () => {
               <div>
                 <p>Demo User:</p>
                 <p>username: demoUser</p>
-                <p>password: defaultPassword123!</p>
+                <p>password: demoPassword123!</p>
               </div>
 
               <div>
                 <p>Demo Admin:</p>
                 <p>username: demoAdmin</p>
-                <p>password: defaultPassword123!</p>
+                <p>password: demoPassword123!</p>
               </div>
 
               <div>
                 <p>Demo Manager:</p>
                 <p>username: demoManager</p>
-                <p>password: defaultPassword123!</p>
+                <p>password: demoPassword123!</p>
               </div>
             </div>
           </CardContent>
